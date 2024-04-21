b718c9d6896f6bb5deedd4f27f6428112ce0836b
AtefMokhtari <165385865+AtefMokhtari@users.noreply.github.com>
2024-04-22 00:03:18 +0200

    Update RestMotPasse.cy.js

--- cypress/e2e/RestMotPasse.cy.js
+++ cypress/e2e/RestMotPasse.cy.js
@@ -1,24 +1,29 @@
 describe('Mot de pass oublié', () => {
 
   // Test cases pour la forget password
-  it('Reset mot de pass avec succée', () => {
-      cy.visit('https://ecommerce-playground.lambdatest.io/')
-      cy.title().should('eq', 'Your Store')
-      cy.get('a[href="https://ecommerce-playground.lambdatest.io/index.php?route=account/account"]').contains(" My account").click({force:true})   
-      cy.get('form[action="https://ecommerce-playground.lambdatest.io/index.php?route=account/login"]').find('a[href="https://ecommerce-playground.lambdatest.io/index.php?route=account/forgotten"]').click({force:true})
-      cy.contains("Forgot Your Password?")
-      cy.get('input[name="email"]').type("med@test.com")
-      cy.get('button[type="submit"]').contains('Continue').click()
-      cy.contains(' An email with a confirmation link has been sent your email address.')
+  it('a forget password test case', () => {
+    cy.visit('https://ecommerce-playground.lambdatest.io/')
+   cy.title().should('eq', 'Your Store')
+   cy.get('a[href="https://ecommerce-playground.lambdatest.io/index.php?route=account/account"]').contains(" My account").click({force:true})   
+   cy.get('form[action="https://ecommerce-playground.lambdatest.io/index.php?route=account/login"]').find('a[href="https://ecommerce-playground.lambdatest.io/index.php?route=account/forgotten"]').click({force:true})
+   cy.contains("Forgot Your Password?")
+   cy.get('input[name="email"]').type("atef@test.com")
+   cy.get('button[type="submit"]').contains('Continue').click()
+   cy.contains(' An email with a confirmation link has been sent your email address.')
   
   //Ajoutez autres test cases pour la partie forget password
     /*
-    it('a forget password test case', () => {
-      .
-      .
-      .
-      .
-    });
+it('test Reset mot de pass avec un format d'adresse invalide , () => {
+  cy.visit('https://ecommerce-playground.lambdatest.io/')
+  cy.title().should('eq', 'Your Store')
+  cy.get('a[href="https://ecommerce-playground.lambdatest.io/index.php?route=account/account"]').contains(" My account").click({force:true})   
+  cy.get('form[action="https://ecommerce-playground.lambdatest.io/index.php?route=account/login"]').find('a[href="https://ecommerce-playground.lambdatest.io/index.php?route=account/forgotten"]').click({force:true})
+  cy.contains("Forgot Your Password?")
+  cy.get('input[name="email"]').type("randomtext")
+  cy.get('button[type="submit"]').contains('Continue').click()
+  cy.contains('  Warning: The E-Mail Address was not found in our records, please try again!')
+      
+      
     */    
   })
 })
